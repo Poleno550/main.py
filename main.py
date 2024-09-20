a8be7a367300aa54fc8fd1181a52bc889d224114
Poleno550 <polinashpk11@gmail.com>
2024-09-20 14:35:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import math
+
+def main():
+    a = 2
+    b = 3
+    c = 5
+    d = 7
+    
+    numerator = (-3 + a**2)*b - 2**3
+    denominator = c - 2*d
+    result = numerator / denominator
+    print("Результат:", result)
+
+if __name__ == '__main__':
+    main()
